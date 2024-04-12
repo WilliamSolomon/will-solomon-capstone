9644e0afa5af5806f684b35578ea31cd66abab51
William Solomon <wsolomon@live.com>
2024-04-11 20:41:46 -0400

    Add SearchBar feature, test with GEO API

---
+++ src/components/GeoAPI/GeoAPIOptions.js
@@ -0,0 +1,12 @@
+export const GeoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'afe0f62923msh16bda9b835e8f1cp10eb5djsn94ea0a3b73b3',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const geoAPIURL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+
+
