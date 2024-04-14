05fa09b779ab187669471f82ec22af25ea5eddd1
William Solomon <wsolomon@live.com>
2024-04-14 17:04:45 -0400

    Commit unsaved changes

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -5,10 +5,34 @@ import axios from 'axios';
 import SettingsList from '../../components/SettingsList/SettingsList';
 import Header from '../../components/Header/Header';
 
+import deleteIcon from '../../assets/icons/delete_outline-24px.svg';
+import SearchModal from '../../components/SearchModal/SearchModal';
+
 const Settings = () => {
     const navigate = useNavigate();
     const [settingsList, setSettingsList] = useState([]);
+    const [userCity, setUserCity] = useState("Miami-Dade County, Florida, US");
+    const [userCoord, setUserCoord] = useState({ lat: '-80.1937', lon: '25.7743' });
     const userId = '2'; // Temp user_id
+
+    const handleModalToggle = (shouldOpen) => {
+        if (shouldOpen) {
+            document.body.classList.add('no-scroll');
+            openModal();
+        } else {
+            document.body.classList.remove('no-scroll')
+            closeModal()
+        }
+    }
+
+    const [isModalOpen, setModalOpen] = useState(false);
+
+    const openModal = () => setModalOpen(true);
+    const closeModal = () => setModalOpen(false);
+
+    useEffect(() => {
+        handleModalToggle(isModalOpen)
+    }, [isModalOpen])
 
     useEffect(() => {
         const getSettingsList = async () => {
@@ -26,16 +50,46 @@ const Settings = () => {
         navigate('/settings/add');
     }
 
+    const handleSearchChange = (searchData) => {
+        setUserCity(searchData.label);
+        const [lat, lon] = searchData.value.split(" ");
+        setUserCoord({ lat, lon });
+    }
+
     return (
         <>
             <main>
                 <Header />
+
                 <section className="settings">
                     <div className="settings__container">
                         <section className="settings__header">
                             <h1 className="settings__title">Settings</h1>
                             <div className="settings__right">
-                                <input className="settings__search" type="text" placeholder="Search..." />
+
+                                <p className='dashboard__title'>
+                                    CURRENT LOCATION: {userCity}
+                                </p>
+                                <div className="dashboard__actions">
+                                    <SearchModal
+                                        isOpen={isModalOpen}
+                                        onClose={closeModal}
+                                        onConfirm={() => {
+                                            sessionStorage.setItem('user_city', userCity);
+                                            sessionStorage.setItem('user_lat', userCoord.lat);
+                                            sessionStorage.setItem('user_lon', userCoord.lon);
+                                            closeModal();
+                                        }}
+                                        onSearchChange={handleSearchChange} // Pass handleSearchChange as a prop
+                                    />
+
+                                    <img
+                                        className="dashboard__location-update"
+                                        src={deleteIcon}
+                                        alt="update location icon"
+                                        onClick={openModal} />
+                                </div>
+
                                 <button className="settings__button" onClick={handleAddItem}>+ Add New Item</button>
                             </div>
                         </section>
