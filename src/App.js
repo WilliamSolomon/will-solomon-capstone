9c28dbedaf13f2ffd022e5b1a0781eb5bb5add20
William Solomon <wsolomon@live.com>
2024-04-14 13:09:09 -0400

    Add setting page and child components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Dashboard from './pages/Dashboard/Dashboard';
+import Settings from './pages/Settings/Settings';
 // import Header from './components/Header/Header';
 // import Delete from './pages/Delete/Delete';
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Dashboard />}></Route>
         <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route path='/settings' element={<Settings />}></Route>
         {/* <Route path='/delete' element={<Delete />}></Route> */}
       </Routes>
       {/* <Footer /> */}
