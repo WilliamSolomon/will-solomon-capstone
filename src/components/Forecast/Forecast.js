5cffbc7afbcce244a35221afba2a496c704db16f
William Solomon <wsolomon@live.com>
2024-05-02 23:00:33 -0400

    Set search to update location in local storage and useState addressInfo

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,3 +1,59 @@
+// import NavBar from "../NavBar/NavBar";
+// import "./Forecast.scss";
+// import { currentWeatherUrl, forecastWeatherUrl } from '../../components/GeoAPI/GeoAPIOptions';
+// import { useState, useEffect } from 'react';
+// import axios from "axios";
+// import TodayWeather from "../TodayWeather/TodayWeather";
+// import Carousel from "../Carousel/Carousel";
+
+// const Forecast = ({ userLocation }) => {
+//     const [currentWeather, setCurrentWeather] = useState(null);
+//     const [forecastWeather, setForecast] = useState(null);
+//     const [currentCoord, setCurrentCoord] = useState({});
+//     const [currentCity, setCurrentCity] = useState("");
+//     const [isLoading, setIsLoading] = useState(true);
+
+//     useEffect(() => {
+//         console.log("Triggered child", userLocation);
+//     }, [userLocation])
+
+//     useEffect(() => {
+//         // Fetch data when component mounts or when user location changes
+//         const fetchData = async () => {
+//             try {
+//                 const [currentResponse, forecastResponse] = await Promise.all([
+//                     axios.get(`${currentWeatherUrl}/?lat=${currentCoord.lat}&lon=${currentCoord.lon}`),
+//                     axios.get(`${forecastWeatherUrl}/?lat=${currentCoord.lat}&lon=${currentCoord.lon}`)
+//                 ]);
+//                 setCurrentWeather({ city: currentCity, ...currentResponse.data });
+//                 setForecast({ city: currentCity, ...forecastResponse.data });
+//                 setIsLoading(false); // Set loading to false after data retrieval
+//             } catch (error) {
+//                 console.error(error);
+//             }
+//         };
+//         fetchData();
+//     }, []); // Fetch data whenever currentCoord changes
+
+//     if (isLoading) {
+//         return <div>Loading...</div>;
+//     }
+
+//     return (
+//         <section className="forecast">
+//             <NavBar />
+//             <div className="forecast__container">
+//                 <h1 className="forecast__title">Today</h1>
+//                 {/* <TodayWeather weatherData={currentWeather} /> */}
+//                 <h2 className="forecast__sub-title">Daily</h2>
+//                 {/* <Carousel weatherData={forecastWeather} /> */}
+//             </div>
+//         </section>
+//     );
+// };
+
+// export default Forecast;
+
 import NavBar from "../NavBar/NavBar";
 import "./Forecast.scss";
 import { currentWeatherUrl, forecastWeatherUrl } from '../../components/GeoAPI/GeoAPIOptions';
@@ -6,33 +62,64 @@ import axios from "axios";
 import TodayWeather from "../TodayWeather/TodayWeather";
 import Carousel from "../Carousel/Carousel";
 
-const Forecast = () => {
+const Forecast = ({ userLocation }) => {
     const [currentWeather, setCurrentWeather] = useState(null);
     const [forecastWeather, setForecast] = useState(null);
-    const [currentCoord, setcurrentCoord] = useState({lat: 40.7128, lon: -74.0060});
-    const [currentCity, setcurrentCity] = useState(["New York City, New York, US"]);
-    const [token, setToken] = useState(null);
-    const [isLoading, setIsLoading] = useState(true); // New state for loading indicator
+    const [currentCoord, setCurrentCoord] = useState({});
+    const [currentCity, setCurrentCity] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
+        if (userLocation) {
+            setCurrentCity(userLocation.address);
+            setCurrentCoord({ lat: userLocation.lat, lon: userLocation.lng });
+            console.log("Child userLocation", userLocation);
+        }
+    }, [userLocation]);
+
+    // useEffect(() => {
+    //     // Fetch data when component mounts or when user location changes
+    //     const fetchData = async () => {
+    //         try {
+    //             const [currentResponse, forecastResponse] = await Promise.all([
+    //                 axios.get(`${currentWeatherUrl}/?lat=${currentCoord.lat}&lon=${currentCoord.lon}`),
+    //                 axios.get(`${forecastWeatherUrl}/?lat=${currentCoord.lat}&lon=${currentCoord.lon}`)
+    //             ]);
+    //             setCurrentWeather({ city: currentCity, ...currentResponse.data });
+    //             setForecast({ city: currentCity, ...forecastResponse.data });
+    //             setIsLoading(false); // Set loading to false after data retrieval
+    //         } catch (error) {
+    //             console.error(error);
+    //         }
+    //     };
+    //     fetchData();
+    // }, [userLocation]); // Fetch data whenever currentCoord or currentCity changes
+
+    useEffect(() => {
+        // Fetch data when component mounts or when user location changes
+        console.log("REady with userLocation", userLocation);
         const fetchData = async () => {
             try {
-                const [currentResponse, forecastResponse] = await Promise.all([
-                    axios.get(`${currentWeatherUrl}/?lat=${currentCoord.lat}&lon=${currentCoord.lon}`),
-                    axios.get(`${forecastWeatherUrl}/?lat=${currentCoord.lat}&lon=${currentCoord.lon}`)
-                ]);
-                setCurrentWeather({ city: currentCity, ...currentResponse.data });
-                setForecast({ city: currentCity, ...forecastResponse.data });
-                setIsLoading(false); // Set loading to false after data retrieval
+                if (userLocation) {
+                    // const [currentResponse, forecastResponse] = await Promise.all([
+                    //     axios.get(`${currentWeatherUrl}/?lat=${userLocation.lat}&lon=${userLocation.lon}`),
+                    //     axios.get(`${forecastWeatherUrl}/?lat=${userLocation.lat}&lon=${userLocation.lon}`)
+                    // ]);
+                    // setCurrentWeather({ city: currentCity, ...currentResponse.data });
+                    // setForecast({ city: currentCity, ...forecastResponse.data });
+                    // setIsLoading(false); // Set loading to false after data retrieval
+                }
             } catch (error) {
                 console.error(error);
             }
         };
+    
         fetchData();
-    }, []);
+    }, [userLocation]); // Fetch data whenever userLocation changes
+    
 
     if (isLoading) {
-        return <div>Loading...</div>; // Render loading indicator while data is being fetched
+        return <div>Loading...</div>;
     }
 
     return (
@@ -40,9 +127,9 @@ const Forecast = () => {
             <NavBar />
             <div className="forecast__container">
                 <h1 className="forecast__title">Today</h1>
-                <TodayWeather weatherData={currentWeather} />
+                {/* <TodayWeather weatherData={currentWeather} /> */}
                 <h2 className="forecast__sub-title">Daily</h2>
-                <Carousel weatherData={forecastWeather} />
+                {/* <Carousel weatherData={forecastWeather} /> */}
             </div>
         </section>
     );
--- src/pages/Weather/Weather.js
+++ src/pages/Weather/Weather.js
@@ -1,16 +1,57 @@
 // import { currentWeatherUrl, forecastWeatherUrl } from '../../components/GeoAPI/GeoAPIOptions';
-// import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 // import axios from "axios";
 import Forecast from '../../components/Forecast/Forecast';
 import Hero from '../../components/Hero/Hero';
 import "./Weather.scss"
 
 const Weather = () => {
+    const [addressInfo, setAddressInfo] = useState([])
+
+    // useEffect to check localStorage and set addressInfo
+    useEffect(() => {
+        const storedAddress = localStorage.getItem('user_address');
+        const storedLat = localStorage.getItem('user_lat');
+        const storedLng = localStorage.getItem('user_lng');
+
+        if (storedLng) {
+            console.log("Stored Address is present", storedAddress);
+            console.log("Stored Latitude is present", storedLat);
+            console.log("Stored Longitude is present", storedLng);
+            setAddressInfo({
+                address: storedAddress,
+                lat: storedLat,
+                lon: storedLng
+            });
+            console.log("Use effect address info:::", {
+                address: storedAddress,
+                lat: storedLat,
+                lon: storedLng
+            });
+        }
+    }, []); // This useEffect runs only once on component mount
+
+    useEffect(() => {
+        console.log("Parent triggered", addressInfo);
+    }, [addressInfo])
+
+    // useEffect to watch for changes in addressInfo and update local storage
+    useEffect(() => {
+        console.log("Storage update triggered", addressInfo);
+        if (addressInfo.address && addressInfo.lat && addressInfo.lon) {
+            localStorage.setItem('user_address', addressInfo.address);
+            localStorage.setItem('user_lat', addressInfo.lat);
+            localStorage.setItem('user_lng', addressInfo.lon);
+        }
+    }, [addressInfo]);
+
+
+
     return (
         <main className='weather__container'>
             <div className='weather__content'>
-                <Hero></Hero>
-                <Forecast></Forecast>
+                <Hero setAddressInfo={setAddressInfo} userLocation={addressInfo ? addressInfo.address : null} />
+                {addressInfo && <Forecast userLocation={addressInfo} />}
             </div>
         </main>
     )
